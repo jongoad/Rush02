f62b749cc450ad56911391747368c5b7d21de980
Jonathan Goad <jon@weareopensunday.com>
2021-07-24 19:03:20 -0400

    RUSH_2_0.3

--- CHECK_ARGS.c
+++ CHECK_ARGS.c
@@ -1,2 +1,111 @@
 //This file contains the required functions to check if the command line input is valid
 
+
+
+int dict_check
+
+//This function converts the string passed to an integer.
+//If the number is negative we can return a value outside of unsigned int range
+unsigned long   unsign_atoi(char *argv)
+{
+    int nbr;
+    int i;
+
+    i = 0;
+    while (argv[i] >= '0' && argv[i] <= '9')
+    {
+        nbr = (nbr * 10) + (argv[i] - '0');
+        i++;
+    }
+    if (argv[i] < '0' || argv[i] > '9' || nbr > 4294967295)
+        return (5000000000);
+    else 
+        return (nbr);
+}
+
+//If the number is valid return is 1
+//If the number is not valid, return is -1
+int is_arg_nbr_valid(char *argv)
+{
+    unsigned long   nbr;
+
+    nbr = unsign_atoi(argv);
+    if (nbr > 4294967295)
+        return (-1);
+    else 
+        return (1);
+}
+
+//Return -2 if there is a dict error
+//Return -1 if there is an number error, 
+//Return 0 if nbr is valid and there is no reference dict passed
+//Return 1 if nbr is valid and there is a reference dict passed
+int arg_check(int argc,char **argv)
+{
+    int nbr_check;
+    int dict_check;
+
+    //Double check return values for check_dict
+
+    //If 2 arguments are passed we need to check number and dict
+    //Also the position of number is shifted by 1 address
+    if (argc == 3)
+    {
+        nbr_check = is_arg_nbr_valid(argv[2]);
+        dict_check = check_dict(argv[1]);
+    }
+    //If only one argument is passed we simply check the number
+    else if (argc == 2)
+    {
+        nbr_check = is_arg_nbr_valid(argv[1]);
+        dict_check = 0;
+    }
+    else if (argc == 1)
+        return (-1);
+
+    //Return -1 for number error
+    if (nbr_check == -1)
+        return (-1);
+    //Return -2 for dict error
+    else if (dict_check == -1)
+        return (-2);
+    //Return 1 if there is a valid reference dictionary and number
+    else if (dict_check == 1)
+        return (1);
+    //Return 0 if there is only a number
+    else
+        return (0);
+}
+
+
+
+//If there is an error, arg_check will return -1
+//If the number is valid and there is only one argument, it will return 0
+//If the number is valid and there are 2 arguments, it will return 1
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+    int arg_status;
+    //Call a function to check the validity of arguments, as well as telling us if we need to check a command line dictionary
+    arg_status = arg_check(argc, argv);
+    if (arg_status == -2)
+        printf("Dict Error\n");
+    else if (arg_status == -1)
+        printf("Error\n");
+    else if (arg_status == 0)
+        printf("Valid number, no reference dict\n");
+    else if(arg_status == 1)
+        printf("Valid number, reference dict is: %s\n", argv[1]);
+
+    return (0);    
+}
--- TEST_ENV.c
+++ TEST_ENV.c
@@ -1,49 +1,25 @@
-//This file contains the required functions to parse our dictionary
+//THIS FILE IS FOR TESTING STUFF
+
+//HEADERS AND PROTOTYPES HERE
+#include <stdio.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <errno.h>
-//We need to go over the file line by line check rules with a atoi-like function
-//When ever a new line character is reached, we need to reset the atoi-like function to its starting state
-
-//If open() return -1 it is an error and errno to should be set to indicate error
-//Open returns a file descriptor
-/*
-Syntax in C language 
-#include<sys/types.h>
-#include<sys/stat.h>
-#include <fcntl.h>  
-int open (const char* Path, int flags [, int mode ]);
-
-File descriptor is the return value from open()
-
-By default the return value is 3.
-
-This is because 0, 1, and 2 are reserved.
-- 0 is read
-- 1 is write
-- 2 is write to error
-*/
-
-/****************************************************************************************************************************/
-
-//Not sure if we need/want to use this? I think it stores the current error code to be returned
-//extern int errno;
-
+#include <stdlib.h>
 
 // Function to find the size of the file so we can malloc the correct size memory block.
-int get_file_size(fd)
+int get_file_size(void)
 {
     int count; //File size counter
     int fd; //File descriptor
     int status; //Loop control using return value from read, also failure status of whole function
-    char    c; //Temporary buffer to hold read bytes in local memory
+    char    *c; //Temporary buffer to hold read bytes in local memory
 
     count = 0;
     status = 1;
 
     //Create the memory location for buffer
-    c = (char)malloc(sizeof(char));
+    c = (char *)malloc(sizeof(char) + 1);
 
     //Open the file
     fd = open("numbers.dict", O_RDONLY);
@@ -54,6 +30,7 @@ int get_file_size(fd)
         if (status > 0)
             count++;
     }
+    
     //Free the allocated memory
     free(c);
 
@@ -74,21 +51,23 @@ char *file_to_string(void)
     int fd;
     int status;
     char *str;
+    int size;
 
     status = 1;
 
     //Allocate a memory block large enough to hold read output, including an extra byte for null terminator.
-    str = (char *)malloc(sizeof(char) * (get_file_size() + 1));
+    size = get_file_size();
+    str = (char *)malloc(sizeof(char) * (size + 1));
+
     //Open the file
     fd = open("numbers.dict", O_RDONLY);
 
     //Start reading the file 1 byte at a time until the return from read() is 0 (end of file) or an error is returned (-1)
     //Returns above 0 are the number of bytes read
-    while (status > 0 && fd != -1)
-    {
-        status = read(fd, str, 1);
-        str++;
-    }
+    status = read(fd, str, size);
+   
+    //Null terminate the string
+    str[size] = '\0';
 
     //Close the file
     status = close(fd);
@@ -100,7 +79,12 @@ char *file_to_string(void)
         return (str);
 }
 
-int parse_main()
+int main(void)
 {
+    char *str;
 
+    str = file_to_string();
+
+    printf("%s", str);
+    return (0);
 }
